0c8f975344e9fad00f73dda821417f5c9c6fd868
michaelcodes <michaelletsgo@Michaels-MBP.lan1>
2021-03-15 23:50:00 -0400

    created person class

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+//HAMSTER
 class Hamster{
     owner = "";
     price = 15;
@@ -11,10 +12,58 @@ class Hamster{
         console.log("nibble nibble");
     }
     getPrice(){
-        return console.log(this.price);
+        return this.price;
     }
 }
 
 const newHamster = new Hamster ("Cuy");
 // console.log(newHamster);
-newHamster.getPrice();
+console.log(newHamster.getPrice());
+
+//PERSON
+class Person {
+    constructor(name){
+        this.name = name;
+    }
+    age = 0;
+    height = 0;
+    weight = 0;
+    mood = 0;
+    hamsters = [];
+    bankAccount = 0;
+    getName(){
+        return this.name;
+    }
+    getAge(){
+        return this.age;
+    }
+    getWeight(){
+        return this.weight;
+    }
+    greet(){
+        console.log(`hello ${this.name}`)
+    }
+    eat(){
+        this.weight++;
+        this.mood++;
+    }
+    exercise(){
+        this.weight--;
+    }
+    ageUp(){
+        this.age++;
+    }
+    buyHamster(hamster){
+        
+        this.hamsters.push(hamster)
+        this.mood += 10;
+        this.bankAccount -= newHamster.getPrice()
+    
+
+    }
+}
+const per = new Person ("Michael");
+// console.log(newHamster);
+per.buyHamster("Cuy");
+console.log(per);
+
