304bac2f36f66313cb94f26a451c8f06e5f91fbc
michaelcodes <michaelletsgo@Michaels-MBP.lan1>
2021-03-16 00:19:32 -0400

    created timmys story

--- objects.js
+++ objects.js
@@ -16,9 +16,9 @@ class Hamster{
     }
 }
 
-const newHamster = new Hamster ("Cuy");
+
 // console.log(newHamster);
-console.log(newHamster.getPrice());
+
 
 //PERSON
 class Person {
@@ -62,8 +62,51 @@ class Person {
 
     }
 }
-const per = new Person ("Michael");
-// console.log(newHamster);
-per.buyHamster("Cuy");
-console.log(per);
+const per = new Person ("Timmy");
+per.age = 5;
 
+
+//Create function eating
+function bummedEat(num){
+    for(let i = 0; i < num; i++){
+        per.eat();
+    }
+    return per.weight;
+}
+
+bummedEat(5);
+console.log(`After having a bummed eating session Timmy gained ${per.weight} pounds!`);
+//Created exercise function
+function exerciseTim(num){
+    for(let i = 0; i < num; i++){
+        per.exercise();
+    }
+    return per.exercise;
+}
+exerciseTim(5);
+console.log(`To look good, in preparation for kindergation, Timmy exercised. 
+His total weight gain is now ${per.weight}!`);
+
+function ageTim(num){
+    for(let i = 0; i < num; i++){
+        per.ageUp();
+    }
+    return per.ageUp;
+}
+
+ageTim(4);
+console.log(`4 years have passed, Timmy's is now ${per.age} years old.`)
+
+const newHamster = new Hamster ("Gus");
+// console.log(newHamster);
+per.buyHamster("Gus");
+console.log("New Hamster Price: " + newHamster.getPrice());
+console.log(per);
+console.log(per.hamsters[0])
+console.log(`Timmy bought a new hamster for $${newHamster.getPrice()}, his name is ${per.hamsters[0]}.`)
+ageTim(6);
+console.log(`Timmy is now ${per.age} years old.`)
+
+bummedEat(2);
+exerciseTim(2);
+console.log(`Timmy exercised and ate twice, so far he has gained ${per.weight} pounds, yet his mood up to ${per.mood}`);
